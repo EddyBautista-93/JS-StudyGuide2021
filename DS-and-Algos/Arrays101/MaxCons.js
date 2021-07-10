9c781a0254e47700ffa31dcc73e1429299a7dde8
EddyBautista <bautista.j.eddy@gmail.com>
2021-07-10 09:03:17 -0500

    moved files over from other js guide

--- DS-and-Algos/Arrays101/MaxCons.js
+++ DS-and-Algos/Arrays101/MaxCons.js
@@ -1,6 +1,6 @@
 // Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
-
+ 
 
 // Example 1:
 
@@ -13,22 +13,18 @@
 // Output: 2
 
 const findMaxConsecutiveOnes = (nums) => {
-    // initialize count & result
     let count = 0;
     let result = 0;
-
     for (let i = 0; i < nums.length; i++) {
-        // reset counter when 0 is found
         if (nums[i] === 0) {
             count = 0;
-        // When 1 is found, increment count and update results
         } else {
             count++
             result = Math.max(result, count)
         }
     }
+    console.log(result);
     return result;
-}
+};
 
-
-findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1]);
+findMaxConsecutiveOnes([1,0,1,1,,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1]);
